98d5be0f12df38b9657f1d682d35a5ad80d00236
Victordami <73849877+victordami01@users.noreply.github.com>
2025-02-06 20:41:23 +0100

    Without menu

--- network.py
+++ network.py
@@ -1,32 +1,29 @@
 import socket
+import pickle
+
 
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "192.168.137.30"  # Make sure this is the correct IP of the server
+        self.server = "192.168.137.30"  # 192.168.8.186 Make sure this is the correct IP of the server
         self.port = 5555
         self.addr = (self.server, self.port)
-        self.pos = self.connect()
+        self.p = self.connect()
+
+    def getP(self):
+        return self.p
 
     def connect(self):
         try:
             self.client.connect(self.addr)
             return self.client.recv(2048).decode()
         except:
-            print("Failed to connect to server")
-            return "0,0"
+            pass
 
     def send(self, data):
         try:
             self.client.send(str.encode(data))
-            return self.client.recv(2048).decode()
+            return pickle.loads(self.client.recv(2048))
         except socket.error as e:
             print(e)
-            return "0,0"
 
-    def getPos(self):
-        return self.pos
-
-if __name__ == "__main__":
-    n = Network()
-    print("Connected to server, received:", n.getPos())
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socket
 from _thread import *
-import sys
+from game import Game
+import pickle
 
 server = "192.168.137.30"  # Accept connections from any IP
 port = 5555
@@ -10,51 +11,66 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
     s.bind((server, port))
 except socket.error as e:
-    print(f"Binding error: {e}")
-    sys.exit()  # Exit if binding fails
+    str(e)
 
-s.listen(2)
+s.listen()
 print("Waiting for a connection, Server Started")
 
-def read_pos(string):
-    try:
-        x, y = map(int, string.split(","))
-        return x, y
-    except ValueError:
-        return 0, 0  # Default if parsing fails
+connected = set()
+games = {}
+idCount = 0
 
-def make_pos(tup):
-    return f"{tup[0]},{tup[1]}"
 
-pos = [(0, 0), (100, 100)]
+def threaded_client(conn, p, gameId):
+   global idCount
+   conn.send(str.encode(str(p)))
 
-def threaded_client(conn, player):
-    try:
-        conn.send(str.encode(make_pos(pos[player])))
+   reply = ""
+   while True:
+       try:
+           data = conn.recv(4096).decode()
 
-        while True:
-            data = conn.recv(2048).decode()
-            if not data:
-                print("Disconnected")
-                break
+           if gameId in games:
+               game = games[gameId]
 
-            pos[player] = read_pos(data)
-            reply = pos[1] if player == 0 else pos[0]
+               if not data:
+                   break
+               else:
+                   if data == "reset":
+                       game.resetWent()
+                   elif data != "get":
+                       game.play(p, data)
 
-            print(f"Received: {data}, Sending: {reply}")
-            conn.sendall(str.encode(make_pos(reply)))
+                   reply = game
+                   conn.sendall(pickle.dumps(reply))
+           else:
+               break
+       except:
+           break
 
-    except Exception as e:
-        print(f"Error in thread: {e}")
+   print("Lost connection")
+   try:
+       del games[gameId]
+       print("Closing Game", gameId)
+   except:
+       pass
+   idCount -= 1
+   conn.close()
 
-    print("Lost connection")
-    conn.close()
 
-currentPlayer = 0
 
 while True:
     conn, addr = s.accept()
     print("Connected to", addr)
 
-    start_new_thread(threaded_client, (conn, currentPlayer))
-    currentPlayer += 1
+    idCount += 1
+    p = 0
+    gameId = (idCount - 1)//2
+    if idCount % 2 == 1:
+        games[gameId] = Game(gameId)
+        print("Creating a new game...", gameId)
+    else:
+        games[gameId].ready = True
+        p = 1
+
+    start_new_thread(threaded_client, (conn, p, gameId))
